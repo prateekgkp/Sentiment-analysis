efa668ac04bd169d1badd2f9c79aface5aab7000
marineprateek <prateekrlinsbits@gmail.com>
2020-05-11 21:55:47 +0530

    uploaded books and converted it into a tidy format.

--- Sentiment-analysis.R
+++ Sentiment-analysis.R
@@ -1,3 +1,8 @@
 library(tidytext) #tidytext package contains sentiments lexicons
-get_sentiments("bing")  #load sentiment lexicon bing 
-
+get_sentiments("bing")  #load sentiment lexicon bing
+library(janeaustenr)
+library(stringr) #stringr package lets us use the pipe operator(%>%) 
+tidy_data<- austen_books()%>%group_by(book)%>% #austen_books() gets a tibble of  books
+  mutate(linenumber=row_number(),
+         chapter=cumsum(str_detect(text,regex("^chapter [\\divxcl]", #\\d includes all decimal digits
+                                              ignore_case = T)))) #ignorecase considers both small and big case
