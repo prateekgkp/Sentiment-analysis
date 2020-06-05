f7a7ea210e234cb66dc8a204574886bc6828d57b
marineprateek <prateekrlinsbits@gmail.com>
2020-06-05 20:08:43 +0530

    unnest token tokenizes each word in a row

--- Sentiment-analysis.R
+++ Sentiment-analysis.R
@@ -1,8 +1,10 @@
 library(tidytext) #tidytext package contains sentiments lexicons
-get_sentiments("bing")  #load sentiment lexicon bing
+get_sentiments("bing")
 library(janeaustenr)
 library(stringr) #stringr package lets us use the pipe operator(%>%) 
 tidy_data<- austen_books()%>%group_by(book)%>% #austen_books() gets a tibble of  books
   mutate(linenumber=row_number(),
          chapter=cumsum(str_detect(text,regex("^chapter [\\divxcl]", #\\d includes all decimal digits
-                                              ignore_case = T)))) #ignorecase considers both small and big case
+                                              ignore_case = T))))%>% #ignorecase considers both small and big case
+ungroup()%>%
+  unnest_tokens(word,text)
